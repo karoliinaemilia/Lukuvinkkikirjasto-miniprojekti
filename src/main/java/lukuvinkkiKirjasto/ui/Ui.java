63e3fe63fa30652177ee6515ee91c0836c7bf6f8
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-11-17 23:17:14 +0200

    web ui hahmottelua

--- src/main/java/lukuvinkkiKirjasto/ui/Ui.java
+++ src/main/java/lukuvinkkiKirjasto/ui/Ui.java
@@ -1,18 +1,36 @@
 
 package lukuvinkkiKirjasto.ui;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.util.Date;
+import java.util.HashMap;
+import lukuvinkkiKirjasto.dao.KirjaDao;
+
+import lukuvinkkiKirjasto.database.Database;
+import lukuvinkkiKirjasto.domain.Kirja;
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Ui {
     
     public static void main(String[] args) throws Exception {
         
-        Spark.get("/hei", (req, res) -> {
-            return "Hei maailma!";
+        Database db = new Database("jdbd:sqlite:LukuvinkkiKirjasto.db");
+        
+        KirjaDao kirjaDao = new KirjaDao(db);
+        
+        Spark.get("/kirjat", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("kirjat", kirjaDao.findAll());
+
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
+       
+        
+        Spark.post("/kirjat", (req, res) -> {
+            kirjaDao.saveOrUpdate(new Kirja(req.queryParams("genre"), req.queryParams("nimi"), Integer.parseInt(req.queryParams("pituus")), req.queryParams("linkki"), req.queryParams("tekija"), new Date()));
+            res.redirect("kirjat");
+            return "";
         });
     }
     
