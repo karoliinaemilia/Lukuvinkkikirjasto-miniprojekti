8feb0bf9293e722e02b72a20582a09981910a320
Martin Strohm <martin.strohm@helsinki.fi>
2018-11-18 18:47:42 +0200

    lisäsin databaseen id sarakkeen, tein dao toiminnallisuuden kirja luokalle ja täydensin kirjaluokkaa

--- src/main/java/lukuvinkkiKirjasto/domain/Kirja.java
+++ src/main/java/lukuvinkkiKirjasto/domain/Kirja.java
@@ -1,28 +1,90 @@
 package lukuvinkkiKirjasto.domain;
 
+import java.time.LocalDate;
 import java.util.Date;
 
-
 public class Kirja {
-    
+
+    private int id;
     private String genre;
     private String nimi;
     private int pituus;
     private String linkki;
     private String tekija;
-    private Date paivamaara;
-    
-    public Kirja(String genre2, String nimi2, int pituus2, String linkki2, String tekija2, Date paivamaara2) {
-        
-        this.genre = genre2;
-        this.nimi = nimi2; 
-        this.pituus = pituus2;
-        this.linkki = linkki2;
-        this.tekija = tekija2;
-        this.paivamaara = paivamaara2;
-                
+    private LocalDate paivamaara;
+
+    public Kirja(int id, String genre, String nimi, int pituus, String linkki, String tekija, LocalDate paivamaara) {
+        this.id = id;
+        this.genre = genre;
+        this.nimi = nimi;
+        this.pituus = pituus;
+        this.linkki = linkki;
+        this.tekija = tekija;
+        this.paivamaara = paivamaara;
+
     }
-    
-    
-    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public int getPituus() {
+        return pituus;
+    }
+
+    public void setPituus(int pituus) {
+        this.pituus = pituus;
+    }
+
+    public String getLinkki() {
+        return linkki;
+    }
+
+    public void setLinkki(String linkki) {
+        this.linkki = linkki;
+    }
+
+    public String getTekija() {
+        return tekija;
+    }
+
+    public void setTekija(String tekija) {
+        this.tekija = tekija;
+    }
+
+    public LocalDate getPaivamaara() {
+        return paivamaara;
+    }
+
+    public void setPaivamaara(LocalDate paivamaara) {
+        this.paivamaara = paivamaara;
+    }
+
+    public String toString() {
+        String tuloste = getNimi() + ", Kirjailija: " + getTekija() + ", julkaistu: " + getPaivamaara() + ", pituus: " + getPituus() + ", genre: " + getGenre();
+        if (!linkki.isEmpty()) {
+            tuloste += ", linkki: " + getLinkki();
+        }
+        return tuloste;
+    }
 }
