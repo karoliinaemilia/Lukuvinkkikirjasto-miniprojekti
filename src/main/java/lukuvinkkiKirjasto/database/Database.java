53b16a1203a223ca3bc533a1d0534da4b2cc720e
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-11-22 04:12:39 +0200

    herokua

--- src/main/java/lukuvinkkiKirjasto/database/Database.java
+++ src/main/java/lukuvinkkiKirjasto/database/Database.java
@@ -11,6 +11,11 @@ public class Database {
     }
 
     public Connection getConnection() throws SQLException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && dbUrl.length() > 0) {
+            return DriverManager.getConnection(dbUrl);
+        }
+        
         return DriverManager.getConnection(databaseAddress);
     }
     
